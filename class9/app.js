2590ea388e112093a214b6fca58d1d98ae662845
zacks <bluemilk1995@gmail.com>
2018-05-25 18:43:49 +0800

    update

--- class9/app.js
+++ class9/app.js
@@ -1,6 +1,9 @@
 const app = require('http').createServer(handler)
 const io = require('socket.io')(app);
 const fs = require('fs');
+const jquery = require('jquery');
+
+let  yee = "test1";
 
 function handler (req, res) {
   fs.readFile(__dirname + '/index.html',
@@ -9,15 +12,23 @@ function handler (req, res) {
       res.writeHead(500);
       return res.end('Error loading index.html');
     }
-
     res.writeHead(200);
     res.end(data);
   });
-}
+};
 
-io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  setInterval( () => {
+    socket.emit('news', { 'hello': yee });
+  });
+  socket.on('myText', data => {
+    console.log(data);
+  });
+  socket.on('yes', data => {
+    console.log(data);
+  });
+  socket.on('no', data => {
     console.log(data);
   });
 });
